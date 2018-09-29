4a0e5b4fec92d3daaf0a56af1b24fd5bf6c41314
André Jr Dorneles <andre.jr.dorneles@gmail.com>
2018-09-29 16:30:36 -0300

    feat(menu): adiciona funcionalidade de navegação

---
+++ CWIEstacionamento/app/src/main/java/br/com/cwi/cwiestacionamento/activities/MainActivity.kt
@@ -0,0 +1,50 @@
+package br.com.cwi.cwiestacionamento.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
+import br.com.cwi.cwiestacionamento.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            mainDrawerLayout.openDrawer(GravityCompat.START)
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var intent = Intent()
+
+        when(item.itemId){
+            R.id.nav_vagas -> {
+                intent = Intent(this, MainActivity::class.java)
+            }
+
+
+            else -> { return false }
+        }
+
+        mainDrawerLayout.closeDrawers()
+        startActivity(intent)
+        return true
+    }
+}
