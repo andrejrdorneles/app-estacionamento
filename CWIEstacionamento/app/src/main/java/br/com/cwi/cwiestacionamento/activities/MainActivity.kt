4e82f115f94e92c32906b59dbc87e35e9d5a91b9
Diuly Barreto <diulybarreto@hotmail.com>
2018-09-29 16:12:14 -0300

    feat/ adicionar logo e opção para deslogar

--- CWIEstacionamento/app/src/main/java/br/com/cwi/cwiestacionamento/activities/MainActivity.kt
+++ CWIEstacionamento/app/src/main/java/br/com/cwi/cwiestacionamento/activities/MainActivity.kt
@@ -1,13 +1,31 @@
 package br.com.cwi.cwiestacionamento.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import br.com.cwi.cwiestacionamento.R
+import br.com.cwi.cwiestacionamento.utils.UserHolder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.logout) {
+            UserHolder.logOut(this)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
