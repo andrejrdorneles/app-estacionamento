07b1261777cb4526db1a078b49ca928ce68de080
Diuly Barreto <diulybarreto@hotmail.com>
2018-10-13 14:35:38 -0300

    fix/ remover import não utilizado

--- CWIEstacionamento/app/src/main/java/br/com/cwi/cwiestacionamento/presenters/LoginPresenter.kt
+++ CWIEstacionamento/app/src/main/java/br/com/cwi/cwiestacionamento/presenters/LoginPresenter.kt
@@ -9,7 +9,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginPresenter(private val view: LoginView) {
